cdb961552adce6c49645ed6fcd764fb8a3d4167e
aryansoni210209-netizen <aryansoni210209@gmail.com>
2026-05-31 08:51:48 +0530

    Update dectobin.cpp

--- dectobin.cpp
+++ dectobin.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
+   
     int i = 0 , ans = 0;
 
     while(n != 0 ) 
@@ -16,4 +17,5 @@ int main(){
 
     }
     cout << ans << endl;
+    
 }
